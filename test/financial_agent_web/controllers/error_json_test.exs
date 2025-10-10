14cef4965f2f5efaf20f7ad8e612e124c0e0c5a3
Ramon de Lima Ramos <ramonlimaramos@gmail.com>
2025-10-10 16:40:09 -0300

    init: init mix app

---
+++ test/financial_agent_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule FinancialAgentWeb.ErrorJSONTest do
+  use FinancialAgentWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert FinancialAgentWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert FinancialAgentWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
