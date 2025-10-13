5f342ab4aa0bfdb189a09c7667b6f0f7296c198f
Ramon de Lima Ramos <ramonlimaramos@gmail.com>
2025-10-13 07:58:57 -0300

    feat: adds front-end app with liveview

--- lib/financial_agent_web/controllers/page_controller.ex
+++ lib/financial_agent_web/controllers/page_controller.ex
@@ -1,9 +1,40 @@
 defmodule FinancialAgentWeb.PageController do
   use FinancialAgentWeb, :controller
 
+  alias FinancialAgent.Accounts
+
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    user_id = get_session(conn, :user_id)
+
+    case user_id do
+      nil ->
+        # User not logged in - show login page
+        render(conn, :home,
+          layout: false,
+          user: nil,
+          google_connected: false,
+          hubspot_connected: false
+        )
+
+      user_id ->
+        # User logged in - check credentials
+        google_connected = has_credential?(user_id, "google")
+        hubspot_connected = has_credential?(user_id, "hubspot")
+        user = Accounts.get_user(user_id)
+
+        render(conn, :home,
+          layout: false,
+          user: user,
+          google_connected: google_connected,
+          hubspot_connected: hubspot_connected
+        )
+    end
+  end
+
+  defp has_credential?(user_id, provider) do
+    case Accounts.get_credential(user_id, provider) do
+      nil -> false
+      _credential -> true
+    end
   end
 end
--- test/financial_agent_web/controllers/page_controller_test.exs
+++ test/financial_agent_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule FinancialAgentWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Financial Agent"
   end
 end
